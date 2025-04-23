a883f61f2d22f02b0b29ebbc055621e4338cee3f
hemantkumar2323 <freelanceb23@gmail.com>
2025-04-23 17:23:08 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FinanceAgent from './FinanceAgent.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FinanceAgent />
+  </React.StrictMode>
+)
